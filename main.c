8d79847e7ff137bcd7dfb81d2a2f1310a7489164
dqrk0jeste <darkonikoloc@gmail.com>
2024-08-21 18:48:42 +0200

    base game is done

--- main.c
+++ main.c
@@ -1,11 +1,12 @@
+#include <signal.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <time.h>
 #include <stdlib.h>
-#include <signal.h>
 #include <ncurses.h>
 #include <stdbool.h>
 
-#define TICK 10000
+#define TICK 100000
 
 #define MAX_TAIL_LENGTH 256
 
@@ -16,6 +17,8 @@
 #define TILE_WIDTH 2
 #define TILE_HEIGHT 1
 
+int screen_height, screen_width;
+
 enum DIRECTION {
   UP = 0,
   DOWN,
@@ -23,18 +26,24 @@ enum DIRECTION {
   LEFT
 };
 
+enum DIRECTION direction;
+
 bool game_running = false;
 
 int head_y, head_x;
+
 struct tail_part {
   int y, x;
   struct tail_part *next;
+  struct tail_part *prev;
 };
-struct tail_part tail_start = { 0, 0, NULL };
-enum DIRECTION direction;
-int food_y, food_x;
+struct tail_part *tail_start = NULL;
+struct tail_part *tail_end = NULL;
 
-inline int random_between(int min, int max) {
+int food_y, food_x;
+bool has_eaten_food = false;
+
+int random_between(int min, int max) {
     return min + rand() % (max - min + 1);
 }
 
@@ -46,7 +55,7 @@ void draw_tile(int y, int x, chtype c) {
   }
 }
 
-void draw_borders(int screen_height, int screen_width) {
+void draw_borders() {
   for(size_t i = 0; i < screen_width; i++) {
     draw_tile(0, i, ' ' | COLOR_PAIR(WHITE_BLOCK));
     draw_tile(screen_height - 1, i, ' ' | COLOR_PAIR(WHITE_BLOCK));
@@ -56,27 +65,219 @@ void draw_borders(int screen_height, int screen_width) {
     draw_tile(i, 0, ' ' | COLOR_PAIR(WHITE_BLOCK));
     draw_tile(i, screen_width - 1, ' ' | COLOR_PAIR(WHITE_BLOCK));
   }
-
-  refresh();
 }
 
-void handle_resize(int sig) {
-  endwin();
-  refresh();
-  clear();
+/*void handle_resize(int sig) {*/
+/*  endwin();*/
+/*  refresh();*/
+/*  clear();*/
+/**/
+/*  screen_height = getmaxy(stdscr) / TILE_HEIGHT;*/
+/*  screen_width = getmaxx(stdscr) / TILE_WIDTH;*/
+/**/
+/*  draw_borders();*/
+/**/
+/*  game_running = false;*/
+/*  refresh();*/
+/*}*/
 
-  int screen_height = getmaxy(stdscr) / TILE_HEIGHT;
-  int screen_width = getmaxx(stdscr) / TILE_WIDTH;
-
-  draw_borders(screen_height, screen_width);
-}
-
-void init_game(int screen_height, int screen_width) {
+void init_snake() {
   head_y = screen_height / 2;
   head_x = screen_width / 2;
   draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+}
 
-  refresh();
+void generate_food() {
+  bool valid = false;
+
+  while(!valid) {
+    food_y = random_between(1, screen_height - 2);
+    food_x = random_between(1, screen_width - 2);
+
+    if(food_x != head_x || food_y != head_y) {
+      valid = true;
+    }
+
+    for(struct tail_part *part = tail_start; part != NULL && part != tail_end; part = part->next) {
+      if(food_x == part->x && food_y == part->y) {
+        valid = false;
+        break;
+      }
+    }
+  }
+
+  
+  draw_tile(food_y, food_x, ' ' | COLOR_PAIR(RED_BLOCK));
+}
+
+bool check_if_hit_itself() {
+  for(struct tail_part *part = tail_start; part != NULL && part != tail_end; part = part->next) {
+    if(head_y == part->y && head_x == part->x) {
+      return true;
+    }
+  }
+  return false;
+}
+
+bool check_if_hit_wall() {
+  if(head_y == 0 || head_y == screen_height - 1
+    || head_x == 0 || head_x == screen_width - 1
+  ) {
+    return true;
+  }
+  return false;
+}
+
+void handle_game_input(int c) {
+  switch (c) {
+    case 'q':
+      game_running = false;
+      break;
+    case KEY_UP:
+    case 'k':
+      if(direction != DOWN) {
+        direction = UP;
+      }
+      break;
+    case KEY_DOWN :
+    case 'j':
+      if(direction != UP) {
+        direction = DOWN;
+      }
+      break;
+    case KEY_RIGHT:
+    case 'l':
+      if(direction != LEFT) {
+        direction = RIGHT;
+      }
+      break;
+    case KEY_LEFT:
+    case 'h':
+      if(direction != RIGHT) {
+        direction = LEFT;
+      }
+      break;
+  }
+}
+
+void move_snake() {
+  int old_head_y = head_y;
+  int old_head_x = head_x;
+
+  if(has_eaten_food) {
+    has_eaten_food = false;
+    switch(direction) {
+      case UP:
+        head_y--;
+        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+        break;
+      case DOWN:
+        head_y++;
+        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+        break;
+      case RIGHT:
+        head_x++;
+        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+        break;
+      case LEFT:
+        head_x--;
+        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+        break;
+    }
+
+    struct tail_part *new = (struct tail_part*)malloc(sizeof(struct tail_part));
+
+    if(tail_start == NULL) {
+      new->y = old_head_y;
+      new->x = old_head_x;
+      new->next = NULL;
+      new->prev = NULL;
+
+      tail_start = new;
+      tail_end = NULL;
+
+      draw_tile(tail_start->y, tail_start->x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+    } else if(tail_end == NULL) {
+      new->y = old_head_y;
+      new->x = old_head_x;
+      new->next = tail_start;
+      new->prev = NULL;
+
+      tail_start->prev = new;
+      tail_start->next = NULL;
+
+      tail_end = tail_start;
+      tail_start = new;
+
+      draw_tile(tail_start->y, tail_start->x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+    } else {
+      new->y = old_head_y;
+      new->x = old_head_x;
+      new->next = tail_start;
+      new->prev = NULL;
+
+      tail_start->prev = new;
+      tail_start = new;
+
+      draw_tile(tail_start->y, tail_start->x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+    }
+
+    return;
+  }
+
+  switch(direction) {
+    case UP:
+      draw_tile(head_y, head_x, ' ');
+      head_y--;
+      draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+      break;
+    case DOWN:
+      draw_tile(head_y, head_x, ' ');
+      head_y++;
+      draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+      break;
+    case RIGHT:
+      draw_tile(head_y, head_x, ' ');
+      head_x++;
+      draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+      break;
+    case LEFT:
+      draw_tile(head_y, head_x, ' ');
+      head_x--;
+      draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+      break;
+  }
+
+  if(tail_start != NULL && tail_end == NULL) {
+    draw_tile(tail_start->y, tail_start->x, ' ');
+
+    tail_start->y = old_head_y;
+    tail_start->x = old_head_x;
+    tail_start->next = NULL;
+    tail_start->prev = NULL;
+
+    draw_tile(tail_start->y, tail_start->x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+  } else if(tail_end != NULL) {
+    draw_tile(tail_end->y, tail_end->x, ' ');
+
+    tail_end = tail_end->prev;
+    struct tail_part *old_tail_end = tail_end->next;
+    tail_end->next = NULL;
+
+    old_tail_end->y = old_head_y;
+    old_tail_end->x = old_head_x;
+    old_tail_end->prev = NULL;
+    old_tail_end->next = tail_start;
+
+    tail_start->prev = old_tail_end;
+    tail_start = old_tail_end;
+
+    draw_tile(tail_start->y, tail_start->x, ' ' | COLOR_PAIR(GREEN_BLOCK));
+  }
+}
+
+bool check_if_eaten_food() {
+  return head_x == food_x && head_y == food_y;
 }
 
 int main() {
@@ -87,6 +288,7 @@ int main() {
   keypad(stdscr, true);
   noecho();
   curs_set(false);
+  /*signal(SIGINT, handle_interupt);*/
 
   start_color();
   use_default_colors();
@@ -100,64 +302,86 @@ int main() {
   init_pair(GREEN_BLOCK, COLOR_GREEN, COLOR_GREEN);
   init_pair(RED_BLOCK, COLOR_RED, COLOR_RED);
 
-  int screen_height = getmaxy(stdscr) / TILE_HEIGHT;
-  int screen_width = getmaxx(stdscr) / TILE_WIDTH;
 
-  draw_borders(screen_height, screen_width);
-
-  signal(SIGWINCH, handle_resize);
-
-  init_game(screen_height, screen_width);
-  
-  int c;
   while(true) {
-    c = getch();
-    if(c == KEY_UP || c == 'k') {
-      direction = UP;
-      break;
-    } else if(c == DOWN || c == 'j') {
-      direction = KEY_DOWN;
-      break;
-    } else if(c == RIGHT || c == 'l') {
-      direction = RIGHT;
-      break;
-    } else if(c == KEY_LEFT || c == 'h') {
-      direction = LEFT;
-      break;
-    }
-  }
-  game_running = true;
+    screen_height = getmaxy(stdscr) / TILE_HEIGHT;
+    screen_width = getmaxx(stdscr) / TILE_WIDTH;
 
-  while(game_running) {
-    switch(direction) {
-      case UP: {
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(-1));
-        head_y--;
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
-        break;
-      }
-      case DOWN: {
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(-1));
-        head_y++;
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
-        break;
-      }
-      case RIGHT: {
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(-1));
-        head_x++;
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
-        break;
-      }
-      case LEFT: {
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(-1));
-        head_x--;
-        draw_tile(head_y, head_x, ' ' | COLOR_PAIR(GREEN_BLOCK));
-        break;
+    clear();
+    nodelay(stdscr, false);
+
+    head_y = 0;
+    head_x = 0;
+    tail_start = NULL;
+    tail_end = NULL;
+    food_y = 0;
+    food_x = 0;
+    has_eaten_food = false;
+
+    draw_borders();
+    init_snake();
+    generate_food();
+
+    refresh();
+
+    int c;
+    bool valid_input = false;
+    while(!valid_input) {
+      c = getch();
+      switch (c) {
+        case 'q': 
+          endwin();
+          return 0;
+        case KEY_UP:
+        case 'k':
+          direction = UP;
+          valid_input = true;
+          break;
+        case KEY_DOWN :
+        case 'j':
+          direction = DOWN;
+          valid_input = true;
+          break;
+        case KEY_RIGHT:
+        case 'l':
+          direction = RIGHT;
+          valid_input = true;
+          break;
+        case KEY_LEFT:
+        case 'h':
+          direction = LEFT;
+          valid_input = true;
+          break;
       }
     }
-    usleep(TICK);
-    refresh();
+
+    game_running = true;
+    nodelay(stdscr, true);
+
+    while(game_running) {
+      c = getch();
+      if (c != -1) {
+        handle_game_input(c);
+      }
+      move_snake();
+      if(check_if_hit_itself() || check_if_hit_wall()) {
+        game_running = false;
+      }
+      if(check_if_eaten_food()) {
+        has_eaten_food = true;
+        generate_food();
+      }
+      refresh();
+      usleep(TICK);
+    }
+
+    struct tail_part *part = tail_start;
+    struct tail_part *tmp;
+    while (part != NULL) {
+       tmp = part;
+       part = part->next;
+       free(tmp);
+    }
   }
-  return 0;
 }
 
